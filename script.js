cb3f0634d8cf0f411b874fb270c87559e5544f43
Gabriel Silveira <d.gabriel36@gmail.com>
2022-10-23 19:04:21 -0300

    Sistema de movimentação com rotação 1 player

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function preload() {
     this.load.image('fundo', 'assets/fundo.jpg');
-    this.load.image('player', 'assets/nave.png');
+    this.load.image('player', 'assets/nave2.png');
     this.load.image('player2', 'assets/pinto.png');
 
     
 }
-var pad1;
-var pad2;
-
+var player;
+var player2;
 
 
 function create() {
@@ -15,30 +14,45 @@ function create() {
     
     
     
-    this.w = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
-    this.a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
-    this.s = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
-    this.d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
-    
+ 
     this.add.image(config.width / 2, config.height / 2, 'fundo');
    
-    this.player = this.physics.add.image(config.width / 2, config.height / 2, 'player').setScale(0.25, 0.25);
-    this.player.setCollideWorldBounds(true);
-    this.player2 = this.physics.add.image(config.width / 2, config.height / 2, 'player').setScale(0.25, 0.25);
-    this.player2.setCollideWorldBounds(true);
+    player = this.physics.add.image(config.width / 2, config.height / 2, 'player').setScale(0.25, 0.25);
+   
+    player.setCollideWorldBounds(true);
+    
+    player.setDrag(100);
+    player.setMaxVelocity(200);
+    player.rotation = 4.71239
+   
 }
 
 function update() {
+   
     let cursors = this.input.keyboard.createCursorKeys();
-    if (( this.a.isDown) || ( this.d.isDown)) this.player.setVelocityX( this.a.isDown ? -160 : 160);
-    else this.player.setVelocityX(0);
-    if (( this.w.isDown) || ( this.s.isDown)) this.player.setVelocityY( this.w.isDown ? -160 : 160);
-    else this.player.setVelocityY(0);
     
-    if ((cursors.left.isDown ) || (cursors.right.isDown )) this.player2.setVelocityX(cursors.left.isDown  ? -160 : 160);
-    else this.player2.setVelocityX(0);
-    if ((cursors.up.isDown)  || (cursors.down.isDown )) this.player2.setVelocityY(cursors.up.isDown  ? -160 : 160);
-    else this.player2.setVelocityY(0);
+    if (cursors.up.isDown)
+    {
+        this.physics.velocityFromRotation(player.rotation,200, player.body.acceleration);
+    }
+    else
+    {
+        player.setAcceleration(0);
+    }
+
+    if (cursors.left.isDown)
+    {
+        player.setAngularVelocity(-300);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setAngularVelocity(300);
+    }
+    else
+    {
+        player.setAngularVelocity(0);
+    }
+
     
    
 }
@@ -51,9 +65,8 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {
-                y:0
-            },
+            fps: 60,
+            gravity: { y: 0 },
             debug: false
         }
     },
