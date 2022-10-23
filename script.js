7c28c925a1c7678df9ae0f860336a00e3f2a7485
Gabriel Silveira <d.gabriel36@gmail.com>
2022-10-23 17:00:59 -0300

    adicionado dois player e sua movimentação no teclado

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function preload() {
-    this.load.image('fundo', 'assets/fundo.jpg')
+    this.load.image('fundo', 'assets/fundo.jpg');
     this.load.image('player', 'assets/nave.png');
+    this.load.image('player2', 'assets/pinto.png');
 
     
 }
+var pad1;
+var pad2;
+
+
 
 function create() {
-
+    
+    
+    
     
     this.w = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
     this.a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
@@ -17,15 +24,23 @@ function create() {
    
     this.player = this.physics.add.image(config.width / 2, config.height / 2, 'player').setScale(0.25, 0.25);
     this.player.setCollideWorldBounds(true);
-    
+    this.player2 = this.physics.add.image(config.width / 2, config.height / 2, 'player').setScale(0.25, 0.25);
+    this.player2.setCollideWorldBounds(true);
 }
 
 function update() {
     let cursors = this.input.keyboard.createCursorKeys();
-    if ((cursors.left.isDown || this.a.isDown) || (cursors.right.isDown || this.d.isDown)) this.player.setVelocityX(cursors.left.isDown || this.a.isDown ? -160 : 160);
+    if (( this.a.isDown) || ( this.d.isDown)) this.player.setVelocityX( this.a.isDown ? -160 : 160);
     else this.player.setVelocityX(0);
-    if ((cursors.up.isDown || this.w.isDown) || (cursors.down.isDown || this.s.isDown)) this.player.setVelocityY(cursors.up.isDown || this.w.isDown ? -160 : 160);
+    if (( this.w.isDown) || ( this.s.isDown)) this.player.setVelocityY( this.w.isDown ? -160 : 160);
     else this.player.setVelocityY(0);
+    
+    if ((cursors.left.isDown ) || (cursors.right.isDown )) this.player2.setVelocityX(cursors.left.isDown  ? -160 : 160);
+    else this.player2.setVelocityX(0);
+    if ((cursors.up.isDown)  || (cursors.down.isDown )) this.player2.setVelocityY(cursors.up.isDown  ? -160 : 160);
+    else this.player2.setVelocityY(0);
+    
+   
 }
 
 const config = {
@@ -37,7 +52,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: {
-                y: 2000
+                y:0
             },
             debug: false
         }
